96a5e75257bb5d89fdf39f2a52a4c1ec361d8c42
Julio Gomes <julio7528@gmail.com>
2023-12-06 15:40:29 -0400

    Fix bug in login functionality

---
+++ Project_Main/__init__.py
@@ -0,0 +1,7 @@
+# import logmessage function from log file Project_Main\submodules\log.py
+from submodules.log import logmessage
+from submodules.log import logStruct
+
+logStruct()
+logmessage("This is a test message")
+logStruct()
